ca81e1c680a2654434709df71d5e558805744f4e
Mahmoud Omara <mahmoud.omara909@gmail.com>
2026-02-13 18:32:36 +0200

    [Feat] Created Codable DTOs

---
+++ Modules/AETour/AETour/Data/Models/DTOs/CityDTO.swift
@@ -0,0 +1,14 @@
+//
+//  CityDTO.swift
+//  AETour
+//
+//  Created by M. Omara on 13/02/2026.
+//
+
+import Foundation
+
+/// Data transfer object for city information from API
+struct CityDTO: Codable {
+    let id: Int
+    let name: String
+}
---
+++ Modules/AETour/AETour/Data/Models/DTOs/ExperienceDTO.swift
@@ -0,0 +1,35 @@
+//
+//  ExperienceDTO.swift
+//  AETour
+//
+//  Created by M. Omara on 13/02/2026.
+//
+
+import Foundation
+
+/// Data transfer object for experience from API
+struct ExperienceDTO: Codable {
+    let id: String
+    let title: String
+    let coverPhoto: String
+    let description: String
+    let viewsNo: Int
+    let likesNo: Int
+    let recommended: Int
+    let hasVideo: Int
+    let city: CityDTO
+    let tourHtml: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case description
+        case city
+        case coverPhoto = "cover_photo"
+        case viewsNo = "views_no"
+        case likesNo = "likes_no"
+        case recommended
+        case hasVideo = "has_video"
+        case tourHtml = "tour_html"
+    }
+}
