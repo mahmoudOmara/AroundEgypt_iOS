b3d41855862d826c845743de8f3a1d7ef19b95ca
Mahmoud Omara <mahmoud.omara909@gmail.com>
2026-02-13 21:28:58 +0200

    [Feat] Implemented AsyncImage as reusable component

---
+++ Modules/AEShared/AEShared/Utilities/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  MPCore
+//
+//  Created by M. Omara on 13/02/2026.
+//
+
+import Foundation
+
+public struct Constants {
+    
+    // MARK: - Development & Testing
+    
+    public struct Development {
+        public static let randomImage = "https://picsum.photos/600/600"
+    }
+}
