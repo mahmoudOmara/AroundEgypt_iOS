32421f7c490da109722ec821171ef0125b07d906
Mahmoud Omara <mahmoud.omara909@gmail.com>
2026-02-13 11:19:00 +0200

    [Feat] created the workspace and added excuteable app target

---
+++ AroundEgyptApp/AroundEgyptApp/AroundEgyptAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundEgyptAppApp.swift
+//  AroundEgyptApp
+//
+//  Created by M. Omara on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AroundEgyptAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
